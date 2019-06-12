a9e8bb50b680c9959b8f6f5aad77773b43a94ebe
imaximciuc <maximciuc.iurii@gmail.com>
2019-06-12 18:12:45 +0300

    add New Appointment components - Client, Service, Date & Time, Staff, Resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppointmentComponent } from './dashboard/appointment/appointment.component';
+import { ClientsComponent } from './dashboard/appointment/clients/clients.component';
+import { ServicesComponent } from './dashboard/appointment/services/services.component';
+import { DateTimeComponent } from './dashboard/appointment/date-time/date-time.component';
+import { StaffComponent } from './dashboard/appointment/staff/staff.component';
+import { ResourcesComponent } from './dashboard/appointment/resources/resources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     DashboardComponent,
-    AppointmentComponent
+    AppointmentComponent,
+    ClientsComponent,
+    ServicesComponent,
+    DateTimeComponent,
+    StaffComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
