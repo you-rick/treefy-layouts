7a6ca07f68e2c262586e7747abdfa7c37f9a8788
imaximciuc <maximciuc.iurii@gmail.com>
2019-06-27 19:40:29 +0300

    Login page styling

--- src/app/layout/authentication/sign-in/sign-in.component.ts
+++ src/app/layout/authentication/sign-in/sign-in.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss', '../authentication.component.scss']
 })
 export class SignInComponent implements OnInit {
 
--- src/app/layout/authentication/sign-up/sign-up.component.ts
+++ src/app/layout/authentication/sign-up/sign-up.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss', '../authentication.component.scss']
 })
 export class SignUpComponent implements OnInit {
 
